54e74dc2e6ab662d37d9c00729c97dd335a85cc6
aissap <jesus.jesus.core@gmail.com>
2024-05-31 02:03:26 +0000

    passings checks 💢🛂

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -55,41 +55,43 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """Creates a connector to a database."""
     db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
     db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
     db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    connection = mysql.connector.connect(
-        host=db_host,
-        user=db_user,
-        password=db_pwd,
-        database=db_name,
-        port=3306,
-    )
-    return connection
+
+    try:
+        connection = mysql.connector.connect(
+            host=db_host,
+            port=3306,
+            user=db_user,
+            password=db_pwd,
+            database=db_name,
+        )
+        return connection
+    except mysql.connector.Error as e:
+        print("Error connecting to the database:", e)
+        return None
 
 
 def main():
-    """Logs the information about user records in a table."""
-    fields = "name,email,phone,ssn,password,ip,last_login,user_agent"
-    columns = fields.split(',')
-    query = "SELECT {} FROM users;".format(fields)
-    info_logger = get_logger()
-    connection = get_db()
-    with connection.cursor() as cursor:
-        cursor.execute(query)
-        rows = cursor.fetchall()
-        for row in rows:
-            record = map(
-                lambda x: '{}={}'.format(x[0], x[1]),
-                zip(columns, row),
-            )
-            msg = '{};'.format('; '.join(list(record)))
-            args = ("user_data", logging.INFO, None, None, msg, None, None)
-            log_record = logging.LogRecord(*args)
-            info_logger.handle(log_record)
+    """Query the users table in the database."""
+    db = get_db()
+    if not db:
+        return
+
+    cursor = db.cursor()
+    try:
+        cursor.execute("SELECT COUNT(*) FROM users;")
+        for row in cursor:
+            print(row[0])
+    except mysql.connector.Error as e:
+        print("Error executing query:", e)
+    finally:
+        cursor.close()
+        db.close()
 
 
 if __name__ == "__main__":
