5ac2a746b689b653e37f97a1f9827101c70def49
aissap <jesus.jesus.core@gmail.com>
2024-05-31 01:53:01 +0000

    passings checks 💢🛂🛂

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,25 +13,17 @@ from typing import List, Tuple
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-def filter_datum(fields, redaction, message, separator):
-    """
-    Obfuscate specified fields in the log message.
-
-    Args:
-        fields: list of strings.
-        redaction: the value by which the fields will be obfuscated.
-        message: string "log line".
-        separator: character used to separate all fields in the log line.
-
-    Returns:
-        log message with obfuscated fields.
-    """
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str) -> str:
+    """Specifie the  fields in the log message."""
     return re.sub(
         r'(?<=^|{})(?:{}=[^{}]+)(?={}|$)'.format(
             separator, '|'.join(fields), separator, separator),
         lambda match: '{}={}'.format(match.group().split('=')[0], redaction),
-        message
-    )
+        message)
 
 
 class RedactingFormatter(logging.Formatter):
