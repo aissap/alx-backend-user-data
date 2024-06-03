70f28137f9b533a1e45859f7107ed78039c51713
aissap <jesus.jesus.core@gmail.com>
2024-06-03 21:48:04 +0200

    requirements ♻♻

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -23,7 +23,20 @@ def filter_datum(
         r'(?<=^|{})(?:{}=[^{}]+)(?={}|$)'.format(
             separator, '|'.join(fields), separator, separator),
         lambda match: '{}={}'.format(match.group().split('=')[0], redaction),
+<<<<<<< Updated upstream
         message)
+=======
+        message
+    )
+
+
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
+    """Filters a log line."""
+    patterns = create_patterns(fields, redaction, separator)
+    return re.sub(patterns['extract'], patterns['replace'], message)
+>>>>>>> Stashed changes
 
 
 class RedactingFormatter(logging.Formatter):
@@ -31,6 +44,10 @@ class RedactingFormatter(logging.Formatter):
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+<<<<<<< Updated upstream
+=======
+    FORMAT_FIELDS = ('name', 'levelname', 'asctime', 'message')
+>>>>>>> Stashed changes
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
@@ -61,6 +78,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
     db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+<<<<<<< Updated upstream
 
     try:
         connection = mysql.connector.connect(
@@ -100,3 +118,38 @@ def main():
 
 if __name__ == "__main__":
     main()
+=======
+    connection = mysql.connector.connect(
+        host=db_host,
+        port=3306,
+        user=db_user,
+        password=db_pwd,
+        database=db_name,
+    )
+    return connection
+
+
+def main():
+    """Logs the information about user records in a table."""
+    fields = "name,email,phone,ssn,password,ip,last_login,user_agent"
+    columns = fields.split(',')
+    query = "SELECT {} FROM users;".format(fields)
+    info_logger = get_logger()
+    connection = get_db()
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            record = map(
+                lambda x: '{}={}'.format(x[0], x[1]),
+                zip(columns, row),
+            )
+            msg = '{};'.format('; '.join(list(record)))
+            args = ("user_data", logging.INFO, None, None, msg, None, None)
+            log_record = logging.LogRecord(*args)
+            info_logger.handle(log_record)
+
+
+if __name__ == "__main__":
+    main()
+>>>>>>> Stashed changes
