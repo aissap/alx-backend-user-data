a01f15568badfeac665fe0298674f2207c0322a5
aissap <jesus.jesus.core@gmail.com>
2024-06-05 00:18:25 +0200

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,10 +10,10 @@ class Auth:
         """checking if authentication is required"""
         if path is None:
             return True
-        if excluded_paths is None or len(excluded_paths) == 0:
+        if excluded_paths is None or not excluded_paths:
             return True
-        for excluded_path in excluded_paths:
-            if path.rstrip('/') == excluded_path.rstrip('/'):
+        for ex_path in excluded_paths:
+            if path.rstrip('/') == ex_path.rstrip('/'):
                 return False
         return True
 
