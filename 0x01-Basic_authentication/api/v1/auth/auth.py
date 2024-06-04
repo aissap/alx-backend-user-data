1489e3fc84cbd3bc23a3f21a93eb6ac90b27536f
aissap <jesus.jesus.core@gmail.com>
2024-06-04 23:19:40 +0200

    require_auth💨💨

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/b`:in/env python3
 """Manage the API authentication.
 """
 from typing import List, TypeVar
@@ -8,14 +8,19 @@ from flask import request
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """checking if authentication is required"""
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+        if path is None:
             return True
-        path = path.rstrip('/') + '/'
-        excluded_paths = [p.rstrip('/') + '/' for p in excluded_paths]
-        return path not in excluded_paths
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        for excluded_path in excluded_paths:
+            if path.rstrip('/') == excluded_path.rstrip('/'):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Retrieving the authorization header from the request."""
+        if request is not None:
+            return request.headers.get('Authorization', None)    
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
