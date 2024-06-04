a5f111cf2c6f857f1509ab6c55f4ab0f9c689b88
aissap <jesus.jesus.core@gmail.com>
2024-06-04 18:31:15 +0200

    api 💨💨

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -35,3 +35,12 @@ def unauthorized() -> None:
       - unauthorized
     """
     abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden() -> None:
+    """ GET /api/v1/forbidden
+    Return:
+      - forbidden
+    """
+    abort(403)
