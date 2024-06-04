9f5e7a9dbf8931df70eaa06af6f84e9d967de235
aissap <jesus.jesus.core@gmail.com>
2024-06-04 22:30:26 +0200

    require_auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,8 +7,12 @@ from flask import request
 
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """checking if authentication """
-        return False
+        """checking if authentication is required"""
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        path = path.rstrip('/') + '/'
+        excluded_paths = [p.rstrip('/') + '/' for p in excluded_paths]
+        return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """Retrieving the authorization header from the request."""
