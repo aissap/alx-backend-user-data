c66457522dc45aa0e59058c8a023b8c09eb7b55a
aissap <jesus.jesus.core@gmail.com>
2024-06-05 02:08:08 +0200

    Fixing

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,9 +14,24 @@ from api.v1.auth.auth import Auth
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
 auth = None
 if os.getenv('AUTH_TYPE') == 'auth':
     auth = Auth()
+
+
+@app.before_request
+def before_request():
+    """Request validation!"""
+    if auth is None:
+        return
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if request.path not in excluded_paths and auth.require_auth(request.path, excluded_paths):
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -37,19 +52,6 @@ def forbidden(error) -> str:
     """forbidden handler
     """
     return jsonify({"error": "forbidden"}), 403
-
-
-@app.before_request
-def before_request():
-    """Request validation!"""
-    if auth is None:
-        return
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
-    if request.path not in excluded_paths and auth.require_auth(request.path, excluded_paths):
-        if auth.authorization_header(request) is None:
-            abort(401)
-        if auth.current_user(request) is None:
-            abort(403)
 
 
 if __name__ == "__main__":
