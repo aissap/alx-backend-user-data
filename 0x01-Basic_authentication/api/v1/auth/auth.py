0b73b65b0212e3e01ae017041de4e2a9a4d528d7
aissap <jesus.jesus.core@gmail.com>
2024-06-05 02:03:20 +0200

    Fixing

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -22,7 +22,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """Return None to indicate that no authorization header is present"""
-        return None
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Return None to indicate that no user is authenticated"""
