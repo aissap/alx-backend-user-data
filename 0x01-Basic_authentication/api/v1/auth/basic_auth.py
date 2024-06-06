b168e97f32466a3d39a872c7bf3c54df0f85554d
aissap <jesus.jesus.core@gmail.com>
2024-06-06 19:58:47 +0200

    auth/basic_auth.py ��

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 import base64
 from api.v1.auth.auth import Auth
 from typing import Optional, Tuple
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -45,3 +46,22 @@ class BasicAuth(Auth):
         
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> Optional[TypeVar('User')]:
+        """Returns  instance based on email and password."""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        
+        users = User.search({'email': user_email})
+        if not users:
+            return None
+        
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+        
+        return user
+    
