5cbce7374907bfd9b26efb7e3fd79531b66b0c75
aissap <jesus.jesus.core@gmail.com>
2024-06-06 23:51:01 +0200

    checking style

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -52,7 +52,9 @@ class BasicAuth(Auth):
         if user_email is None or not isinstance(user_email, str):
             return None
         
-        if user_pwd is None or not isinstance(user_pwd, str):
+        if user_pwd is None:
+            return None
+        elif not isinstance(user_pwd, str):
             return None
         
         users = User.search({'email': user_email})
