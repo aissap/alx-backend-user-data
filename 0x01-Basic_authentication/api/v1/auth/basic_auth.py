ba63ea99e3672e28990cc4c38efd1b55109a7707
aissap <jesus.jesus.core@gmail.com>
2024-06-07 00:27:39 +0200

    checking style

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -31,4 +31,17 @@ class BasicAuth(Auth):
         except Exception:
             return None
 
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> Optional[Tuple[str, str]]:
+        """Extracts user credentials from the decoded value."""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        user_email, user_pwd = decoded_base64_authorization_header.split(':', 1)
+        return user_email, user_pwd
+
         
