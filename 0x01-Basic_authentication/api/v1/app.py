7af0619ae8d759680d047cd7d735a4b2899d8ed7
aissap <jesus.jesus.core@gmail.com>
2024-06-06 19:36:14 +0200

    auth/basic_auth.py ��

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -16,6 +16,7 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
+
 auth_type = os.getenv('AUTH_TYPE')
 
 if auth_type == 'basic_auth':
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,12 +5,14 @@ from typing import Optional
 
 
 class BasicAuth(Auth):
+    """Basic auth."""
     def extract_base64_authorization_header(self, authorization_header: str) -> Optional[str]:
-        """Extracts Base64  of the Authorization header for Basic Authentication."""
-        if authorization_header is None or not isinstance(authorization_header, str):
+        """returns the Base64 part of the Authorization header for a Basic Authentication."""
+        if authorization_header is None:
             return None
-        
-        if not authorization_header.startswith("Basic "):
+        elif not isinstance(authorization_header, str):
+            return None
+        elif not authorization_header.startswith("Basic "):
             return None
         
         return authorization_header.split(" ", 1)[-1].strip()
