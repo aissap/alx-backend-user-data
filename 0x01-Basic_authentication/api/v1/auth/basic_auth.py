eb6ef9a7e5601581f34d7718d25d0ec4d2aa6d2b
aissap <jesus.jesus.core@gmail.com>
2024-06-07 00:00:18 +0200

    edititing style and documentation

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -16,19 +16,18 @@ class BasicAuth(Auth):
             return None
         elif not authorization_header.startswith("Basic "):
             return None
-        
+
         parts = authorization_header.split(" ", 1)
 
         if len(parts) != 2:
             return None
-        
         return parts[1].strip()
 
     def decode_base64_authorization_header(self, base64_authorization_header: str) -> Optional[str]:
         """Returns the decoded value."""
         if base64_authorization_header is None or not isinstance(base64_authorization_header, str):
             return None
-        
+
         try:
             decoded_bytes = base64.b64decode(base64_authorization_header)
             decoded_str = decoded_bytes.decode('utf-8')
@@ -40,10 +39,10 @@ class BasicAuth(Auth):
         """Returns  email and password from the decoded value."""
         if decoded_base64_authorization_header is None or not isinstance(decoded_base64_authorization_header, str):
             return None, None
-        
+
         if ':' not in decoded_base64_authorization_header:
             return None, None
-        
+
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
 
@@ -54,14 +53,14 @@ class BasicAuth(Auth):
         
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
-        
+
         users = User.search({'email': user_email})
         if not users:
             return None
-        
+
         user = users[0]
         if not user.is_valid_password(user_pwd):
             return None
-        
+
         return user
     
