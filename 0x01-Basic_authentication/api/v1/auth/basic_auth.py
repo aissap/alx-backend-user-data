46376ef092ac3321da68c1afe10faf74a3c49599
aissap <jesus.jesus.core@gmail.com>
2024-06-06 19:27:14 +0200

    auth/basic_auth.py ��

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 """BasicAuth module"""
 from api.v1.auth.auth import Auth
+from typing import Optional
 
 
 class BasicAuth(Auth):
-    """Basic authentication class"""
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> Optional[str]:
+        """Extracts Base64  of the Authorization header for Basic Authentication."""
+        if authorization_header is None or not isinstance(authorization_header, str):
+            return None
+        
+        if not authorization_header.startswith("Basic "):
+            return None
+        
+        return authorization_header.split(" ", 1)[-1].strip()
