878e1dfa5f405a375ff40ddbd7e4919d182f686a
aissap <jesus.jesus.core@gmail.com>
2024-06-09 02:45:29 +0200

    session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,10 +2,42 @@
 """ SessionAuth module
 """
 from api.v1.auth.auth import Auth
+from models.user import User
 import uuid
 
 
 class SessionAuth(Auth):
     """Session authentication class
     """
-    pass
+    user_id_by_session_id = {}
+
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates a session id for user """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Returns the user id based on a session id """
+        if session_id is None:
+            return None
+        elif not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ Returns a User instance based on a cookie value """
+        if request is None:
+            return None
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+        user = User.get(user_id)
+        return user
