cac996daa7a39de6af57375b7d0ee88207321d7b
aissap <jesus.jesus.core@gmail.com>
2024-06-13 20:07:27 +0000

    fixing

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
+"""DB module
 """
-DB module
-"""
-
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 
 
@@ -20,6 +16,7 @@ class DB:
         """Initialize a new DB instance
         """
         self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
@@ -33,20 +30,9 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database and return the User object.
+        """Add a new user to the database
         """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
         self._session.commit()
-        return new_user
-
-    def find_user_by(self, **kwargs) -> Optional[User]:
-        """
-        Finds a user by arbitrary keyword arguments.
-        """
-        try:
-            return self._session.query(User).filter_by(**kwargs).one()
-        except NoResultFound:
-            raise NoResultFound("No user found with the given criteria.")
-        except InvalidRequestError:
-            raise InvalidRequestError("Invalid query arguments.")
+        return user
