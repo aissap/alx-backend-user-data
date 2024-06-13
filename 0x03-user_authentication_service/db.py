7c434119ceb5e60de6adaad9a5b6a0703dac695c
aissap <jesus.jesus.core@gmail.com>
2024-06-13 22:19:05 +0200

    fixing

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 
 
@@ -37,13 +39,14 @@ class DB:
         self._session.commit()
         return user
 
-     def find_user_by(self, **kwargs) -> User:
-         """Find a user by arbitrary keyword arguments"""
-         session = self._session
-         try:
-             user = session.query(User).filter_by(**kwargs).one()
-         except NoResultFound:
-             raise NoResultFound("No user found with the specified criteria")
-         except InvalidRequestError:
-             raise InvalidRequestError("Invalid query arguments passed")
-         return user
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by arbitrary keyword arguments
+        """
+        session = self._session
+        try:
+            user = session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise NoResultFound("No user found with the specified criteria")
+        except InvalidRequestError:
+            raise InvalidRequestError("Invalid query arguments passed")
+        return user
