b17c93039fcc55d05b48dd1967062bd310b14af2
aissap <jesus.jesus.core@gmail.com>
2024-06-14 01:34:20 +0200

    task 8️⃣

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -41,8 +41,8 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
             if user is not None:
-                return bcrypt.checkpw(password.encode("utf-8"), user.hashed_password.encode("utf-8"))
+                return bcrypt.checkpw(password.encode('utf-8'),
+                                      user.hashed_password.encode('utf-8'))
         except NoResultFound:
-            return False
-    
+            pass
         return False
