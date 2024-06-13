74349d11d9a46be1a5272b665641cb3087c438f7
aissap <jesus.jesus.core@gmail.com>
2024-06-13 23:58:52 +0000

    task 10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -17,12 +17,12 @@ def _generate_uuid() -> str:
 
 
 def _hash_password(self, password: str) -> bytes:
-        """
-        Hash a password with bcrypt.
-        """
-        salt = bcrypt.gensalt()
-        hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
-        return hashed
+    """
+    Hash a password with bcrypt.
+    """
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed
 
 
 class Auth:
