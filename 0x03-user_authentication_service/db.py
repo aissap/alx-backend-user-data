cf024afd7b89ce63861a543b96f2e4a1bdd3e751
aissap <jesus.jesus.core@gmail.com>
2024-06-13 19:53:38 +0200

    task number 2️⃣

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,30 +2,35 @@
 """
 DB module
 """
-from sqlalchemy import create_engine
+
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
+from typing import Optional
+
 from user import Base, User
 
 
 class DB:
-    """DB class
+    """
+    This class manages the database interaction for the user authentication service.
     """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
+        """
+        Initializes a new DB instance and creates the necessary tables if they do not exist.
         """
         self._engine = create_engine("sqlite:///a.db", echo=True)
-        Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
+        """
+        Memoized session object.
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -33,15 +38,17 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database and return the User object.
+        """
+        Adds a new user to the database.
         """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
 
-    def find_user_by(self, **kwargs) -> User:
-        """Find a user by arbitrary keyword arguments.
+    def find_user_by(self, **kwargs) -> Optional[User]:
+        """
+        Finds a user by arbitrary keyword arguments.
         """
         try:
             return self._session.query(User).filter_by(**kwargs).one()
