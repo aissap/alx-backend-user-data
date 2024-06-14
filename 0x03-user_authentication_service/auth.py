a608c2d9faa74b98b5beb2768617c1e3f7f04b1d
aissap <jesus.jesus.core@gmail.com>
2024-06-14 18:57:53 +0200

    Generate reset password token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -94,3 +94,17 @@ class Auth:
             self._db.update_user(user.id, session_id=None)
         except NoResultFound:
             pass
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generate reset password token.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError(f"User with email '{email}' not found")
+
+        reset_token = str(uuid.uuid4())
+
+        self._db.update_user(user.id, reset_token=reset_token)
+
+        return reset_token
